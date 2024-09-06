73d4109d608214318e574bd8936d99ce461a4792
yihui-hu <yihui.hu@tufts.edu>
2024-09-06 15:51:54 -0400

    Ok whatever footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import "../app/styles/globals.css";
 import "../app/styles/home.css";
 import Header from "./components/Header";
 import Cards from "./components/cards";
-import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Header />
       <Cards />
-      <Footer />
     </div>
   );
 }
