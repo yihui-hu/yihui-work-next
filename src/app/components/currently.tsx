cdae05c36b27637d47cb7c59225b2f1bbb014e2c
yihui-hu <yihui.hu@tufts.edu>
2024-09-06 14:29:16 -0400

    More mobile optimizations

--- src/app/components/currently.tsx
+++ src/app/components/currently.tsx
@@ -15,8 +15,8 @@ export default function Currently() {
       >
         <Image
           src="https://yihui-work.s3.us-east-2.amazonaws.com/sg_sota.webp"
-          width={isMobile ? 106 : 133}
-          height={isMobile ? 80 : 100}
+          width={isMobile ? 94 : 133}
+          height={isMobile ? 72 : 100}
           alt="currently missing summer in Singapore"
           className="currently-img"
           draggable={false}
