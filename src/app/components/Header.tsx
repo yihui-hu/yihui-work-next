8b91cabf6682b1b8cb12cb5bfe0537e448e5d0ba
yihui-hu <yihui.hu@tufts.edu>
2024-09-09 21:47:22 -0400

    I am not sure about this header design

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -32,110 +32,110 @@ export default function Header() {
 
   return (
     <div className="header">
-      <div
-        className="header-card"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 4,
-          transform: "rotate(-2deg)",
-          height: "fit-content",
-        }}
-      >
-        <a style={{ color: "inherit", textDecoration: "none" }} href="/">
-          Yihui Hu
-          <br />
-          <span style={{ color: "#a9a9a9", fontSize: 14 }}>何逸晖</span>
-        </a>
-      </div>
-      <div
-        className="header-card"
-        style={{
-          transform: "rotate(2deg)",
-          paddingInline: 8,
-          paddingBlock: 4,
-          height: "fit-content",
-        }}
-      >
-        <span>
-          I like to design and program. <br />
-          Previously at{" "}
-          <a className="link" href="https://apple.com">
-            Apple
-          </a>{" "}
-          and{" "}
-          <a className="link" href="https://typo.by">
-            Typo*
+      <div style={{ display: "flex", flexDirection: "row", gap: 24 }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 4,
+            transform: "rotate(-2deg)",
+            height: "fit-content",
+          }}
+        >
+          <a style={{ color: "inherit", textDecoration: "none" }} href="/">
+            Yihui Hu
+            <br />
+            <span style={{ color: "#a9a9a9", fontSize: 14 }}>何逸晖</span>
           </a>
-          . <br />
-          <br />
-          Find me on{" "}
-          <a className="link" href="https://are.na/yihui-h">
-            Are.na
-          </a>
-          ,{" "}
-          <a className="link" href="https://github.com/yihui-hu">
-            GitHub
-          </a>
-          , <br />
-          <a className="link" href="https://twitter.com/_yihui">
-            Twitter
-          </a>
-          ,{" "}
-          <a className="link" href="https://linkedin.com/in/yihuihu">
-            LinkedIn
-          </a>
-          , and{" "}
-          <a className="link" href="https://read.cv/yihui">
-            Read.cv
-          </a>
-          .
-          <br />
-          <br />
-          <a className="about-link" href="/about">
-            More about me <RightArrow />
-          </a>
-        </span>
-      </div>
-      <div
-        className="works-index-container header-card"
-        style={{ transform: "rotate(-1deg)" }}
-      >
-        <div style={{ display: "flex", flexDirection: "row", paddingTop: 2 }}>
-          <div className="works-index">
-            {works.slice(0, 9).map((work) => {
-              return (
-                <HoverableWork id={work.id} url={work.url} key={work.id}>
-                  {work.id}{" "}
-                  <span style={{ color: "#b9b9b9" }}>{work.year}</span>
-                </HoverableWork>
-              );
-            })}
-          </div>
-          <div className="works-index">
-            {works.slice(10).map((work) => {
-              return (
-                <HoverableWork id={work.id} url={work.url} key={work.id}>
-                  {work.id}{" "}
-                  <span style={{ color: "#b9b9b9" }}>{work.year}</span>
-                </HoverableWork>
-              );
-            })}
+        </div>
+        <div
+          className="header-card"
+          style={{
+            transform: "rotate(2deg)",
+            paddingInline: 8,
+            paddingBlock: 4,
+            height: "fit-content",
+          }}
+        >
+          <span>
+            I like to design and program. <br />
+            Previously at{" "}
+            <a className="link" href="https://apple.com">
+              Apple
+            </a>{" "}
+            and{" "}
+            <a className="link" href="https://typo.by">
+              Typo*
+            </a>
+            . <br />
+            <br />
+            Find me on{" "}
+            <a className="link" href="https://are.na/yihui-h">
+              Are.na
+            </a>
+            ,{" "}
+            <a className="link" href="https://github.com/yihui-hu">
+              GitHub
+            </a>
+            , <br />
+            <a className="link" href="https://twitter.com/_yihui">
+              Twitter
+            </a>
+            ,{" "}
+            <a className="link" href="https://linkedin.com/in/yihuihu">
+              LinkedIn
+            </a>
+            , and{" "}
+            <a className="link" href="https://read.cv/yihui">
+              Read.cv
+            </a>
+            .
+            <br />
+            <br />
+            <a className="about-link" href="/about">
+              More about me <RightArrow />
+            </a>
+          </span>
+        </div>
+        <div
+          className="works-index-container header-card"
+          style={{ transform: "rotate(-1deg)" }}
+        >
+          <div style={{ display: "flex", flexDirection: "row", paddingTop: 2 }}>
+            <div className="works-index">
+              {works.slice(0, 9).map((work) => {
+                return (
+                  <HoverableWork id={work.id} url={work.url} key={work.id}>
+                    {work.id}{" "}
+                    <span style={{ color: "#b9b9b9" }}>{work.year}</span>
+                  </HoverableWork>
+                );
+              })}
+            </div>
+            <div className="works-index">
+              {works.slice(10).map((work) => {
+                return (
+                  <HoverableWork id={work.id} url={work.url} key={work.id}>
+                    {work.id}{" "}
+                    <span style={{ color: "#b9b9b9" }}>{work.year}</span>
+                  </HoverableWork>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-      {/* <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-        <span>Listening:</span>
-        <Listening />
+      <div style={{ display: "flex", flexDirection: "row", gap: 32 }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+          <Listening />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: 6, transform: "rotate(2deg)" }}>
+          <Reading />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+          <Currently />
+        </div>
       </div>
-      <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
-        <span>Reading:</span>
-        <Reading />
-      </div>
-      <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-        <span>Currently:</span>
-        <Currently />
-      </div> */}
     </div>
   );
 }
